cb39001af2e2852a8c33b3513821e8cf384d80d6
masashi0520 <masashi.gun.0520@gmail.com>
2025-09-18 01:09:42 +0900

    databaseの続きを書いた

--- lib/services/database_helper.dart
+++ lib/services/database_helper.dart
@@ -7,4 +7,60 @@ import '../models/trip.dart';
 class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('realtime_gasorin.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    //車両設定テーブル(TBL-01)
+    await db.execute('''
+    CREATE TABLE VehicleSettings (
+    id INTEGER PRIMARY KEY,
+    tank_capacity REAL NOT NULL,
+    updated_at TEXT NOT NULL
+    )
+    ''');
+
+    //給油履歴テーブルA(TBL-02)
+    await db.execute('''
+    CREATE TABLE FuelRecords (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    refuel data TEXT NOT NULL,
+    fuel_amount REAL NOT NULL,
+    payment_amount INTEGER NOT NULL,
+    distance_since_last_refuel REAL NOT NULL,
+    calculated_fuel_economy REAL NOT NULL,
+    created_at TEXT NOT NULL
+    )
+    ''');
+
+    //走行記録テーブル(TBL-03)
+    await db.execute('''
+    CREATE TABLE Trips (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    trip_data TEXT NOT NULL,
+    distance REAL NOT NULL,
+    created_at TEXT NOT NULL
+    )
+    ''');
+  }
 }
