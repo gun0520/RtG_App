4536a6d51f977bfe50aaf054b30091de81a2af6b
gun <masashi.gun.0520@gmail.com>
2025-09-22 17:48:09 +0900

    給油記録画面を書き始めた

---
+++ lib/screens/fuel_record_input_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '../services/database_helper.dart';
+import '../models/fuel_record.dart';
+
+class FuelRecordInputScreen extends StatefulWidget {
+  const FuelRecordInputScreen({super.key});
+
+  @override
+  State<FuelRecordInputScreen> createState() => _FuelRecordInputState();
+}
